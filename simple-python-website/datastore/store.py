25710caa47eb2c9d158a09c6e0f6dcbdc7d38ac3
nghianx1211 <nghianx1211@gmail.com>
2025-04-12 15:45:05 +0700

    ok

--- simple-python-website/datastore/store.py
+++ simple-python-website/datastore/store.py
@@ -12,7 +12,7 @@ class Store:
 
         file = os.path.join(conf.data_directory, conf.migrations_filename)
         data = None
-        with open(file, 'r') as f:
+        with open(file, "r") as f:
             data = json.load(f)
         if data:
             for m in data["migrations"]:
@@ -25,13 +25,13 @@ class Store:
         with Connection(conf) as db_connection:
             try:
                 c = db_connection.cursor()
-                sql = 'select max(version) from migrations;'
+                sql = "select max(version) from migrations;"
                 row = c.execute(sql).fetchone()
                 if row:
                     schema_version = row[0]
             except Error as err:
                 print(err)
-        print(f'max schema version = {schema_version}')
+        print(f"max schema version = {schema_version}")
         return schema_version
 
     @staticmethod
@@ -50,9 +50,9 @@ class Store:
 
     @staticmethod
     def import_test_data(conf, table, json_data_file):
-        print(f'importing data to {table} from {json_data_file}')
+        print(f"importing data to {table} from {json_data_file}")
         data = None
-        with open(json_data_file, 'r') as f:
+        with open(json_data_file, "r") as f:
             data = json.load(f)
         if data:
             with Connection(conf) as db_connection:
@@ -63,11 +63,12 @@ class Store:
                         values = []
                         for k in columns:
                             values.append(item[k])
-                        cols = f'{columns}'[1:-1]
-                        vals = f'{values}'[1:-1].replace("'CURRENT_TIMESTAMP'", "CURRENT_TIMESTAMP")
-                        sql = f'insert into {table}({cols}) values ({vals});'
+                        cols = f"{columns}"[1:-1]
+                        vals = f"{values}"[1:-1].replace(
+                            "'CURRENT_TIMESTAMP'", "CURRENT_TIMESTAMP"
+                        )
+                        sql = f"insert into {table}({cols}) values ({vals});"
                         print(sql)
                         c.execute(sql)
                 except Error as err:
                     print(err)
-
--- simple-python-website/datastore/users.py
+++ simple-python-website/datastore/users.py
@@ -4,8 +4,8 @@ import hashlib
 def calculate_password_hash(config, password):
     secret = config.password_secret
     module = hashlib.md5()
-    string = f'{password}+{secret}'
-    module.update(bytes(string, encoding='utf-8'))
+    string = f"{password}+{secret}"
+    module.update(bytes(string, encoding="utf-8"))
     password_hash = module.hexdigest()
     return password_hash
 
@@ -13,15 +13,17 @@ def calculate_password_hash(config, password):
 def create_user(db, config, firstname, lastname, username, password):
     password_hash = calculate_password_hash(config, password)
     cursor = db.execute(
-        'INSERT INTO users(firstname, lastname, created_at, is_admin) values(?, ?, CURRENT_TIMESTAMP, 0);',
-        (firstname, lastname))
+        "INSERT INTO users(firstname, lastname, created_at, is_admin) values(?, ?, CURRENT_TIMESTAMP, 0);",
+        (firstname, lastname),
+    )
     row_count1 = cursor.rowcount
     user_id = cursor.lastrowid
 
     if row_count1 > 0:
         row_count2 = db.execute(
             "INSERT INTO auth_methods(user_id, username, password, type) values(?, ?, ?, 'USERNAME_AND_PASSWORD');",
-            (user_id, username, password_hash)).rowcount
+            (user_id, username, password_hash),
+        ).rowcount
 
         if row_count2 > 0:
             return True
@@ -32,10 +34,11 @@ def create_user(db, config, firstname, lastname, username, password):
 def check_login(db, config, username, password):
     password_hash = calculate_password_hash(config, password)
     row = db.execute(
-        'SELECT user_id from auth_methods where username=? and password=? and type=\'USERNAME_AND_PASSWORD\'',
-        (username, password_hash)).fetchone()
+        "SELECT user_id from auth_methods where username=? and password=? and type='USERNAME_AND_PASSWORD'",
+        (username, password_hash),
+    ).fetchone()
     if row:
-        return int(row['user_id'])
+        return int(row["user_id"])
     return -1
 
 
@@ -48,7 +51,7 @@ def check_admin(db, username):
 
 
 def get_user_by_id(db, user_id):
-    row = db.execute('SELECT * from users where id=?', (user_id,)).fetchone()
+    row = db.execute("SELECT * from users where id=?", (user_id,)).fetchone()
     if row:
         user = dict(row)
         return user
@@ -61,7 +64,10 @@ def user_by_username(db, username):
 
 
 def user_id_by_username(db, username):
-    row = db.execute("SELECT user_id from auth_methods where username=? and type='USERNAME_AND_PASSWORD'", (username,)).fetchone()
+    row = db.execute(
+        "SELECT user_id from auth_methods where username=? and type='USERNAME_AND_PASSWORD'",
+        (username,),
+    ).fetchone()
     if row:
         user_id = int(row["user_id"])
         return user_id
